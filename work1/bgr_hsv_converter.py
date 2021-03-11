4dcf9014d8911849ae6425485eb1c166e6650d75
Roman <kisame2002@mail.ru>
2021-03-11 05:23:27 +0300

    +image carousel +size changes +marker color solve v1
    image carousel with preloaded .jpg in /imgs folder. size changes depends on how far object is. 1st try to solve marker color problem

--- work1/bgr_hsv_converter.py
+++ work1/bgr_hsv_converter.py
@@ -1,13 +1,13 @@
 import sys
 import numpy as np
 import cv2
-blue = 6
-green = 195
-red = 229
-color = np.uint8([[[blue, green, red]]])
-hsv_color = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
-hue = hsv_color[0][0][0]
-print("Lower bound is :"),
-print("[" + str(hue-10) + ", 100, 100]\n")
-print("Upper bound is :"),
-print("[" + str(hue + 10) + ", 255, 255]")
+
+
+def get_bounds(color):
+    red = color[0]
+    green = color[1]
+    blue = color[2]
+    color = np.uint8([[[blue, green, red]]])
+    hsv_color = cv2.cvtColor(color, cv2.COLOR_BGR2HSV)
+    hue = hsv_color[0][0][0]
+    return (hue-15, 100, 100), (hue+15, 255, 255)
